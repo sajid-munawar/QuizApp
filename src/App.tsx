667d9f3911ed137621f9536d6f9e0a7df7ac29ee
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-28 19:35:04 +0500

    print question on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,22 @@ function App() {
   useEffect (()=>{
     async function getData(){
       const questions:QuizType[]=await getQuizDetails(5,"easy")
-      console.log(questions)
+      // console.log(questions)
       setQuiz(questions)
 
     }
     getData();
   },[])
+  let aa=quiz
+  console.log({aa})
+  if (quiz.length<2){
+    return <div>Loading...</div>
+  }
   return (
     <div className="App">
+      {quiz[1].question}<br/>
+      {quiz[1].difficulty}
+      {/* {quiz[1].option} */}
     </div>
   );
 }
