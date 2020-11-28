e59b74133f69cf97a30d86beb0d96acb8cbdea1c
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-28 13:48:23 +0500

    call api and console data successfuly

---
+++ src/services/quiz_service.ts
@@ -0,0 +1,10 @@
+// import React from 'react';
+export const getQuizDetails=async (totalQuestions:number,level:string)=>{
+    const res=await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
+    let {results}=await res.json();
+    console.log(results)
+
+    // console.log(data.results[1].question)
+    // console.log(data.results[1].correct_answer)
+    // console.log(data.results[1].incorrect_answers)
+}
